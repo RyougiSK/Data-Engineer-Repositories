93e7c04df62af85ac1548c7212999120adc22803
Hanming Chen <chenhm03@gmail.com>
2022-12-07 14:12:28 +1100

    change sql queries

--- sql_queries.py
+++ sql_queries.py
@@ -41,7 +41,7 @@ user_table_insert = """INSERT INTO users
                     (user_id, first_name, last_name, gender, level) 
                     VALUES (%s, %s, %s, %s, %s) 
                     ON CONFLICT (user_id) 
-                    DO NOTHING"""
+                    DO UPDATE SET level = 'Paid'"""
 
 song_table_insert = """INSERT INTO songs 
                     (song_id, title, artist_id, year, duration) 
